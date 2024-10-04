eed2d443f44945ad1dfd6156f36b144ce2470468
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-10-04 10:04:13 +0000

    [finish]Top/About

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
   get '/about' => 'homes#about'
+  # resources :posts, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
