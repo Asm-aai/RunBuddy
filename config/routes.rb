f8a56863fccb742d3be741b8f50a9ea384e43b51
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-10-05 06:56:54 +0000

    header

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: 'admin/sessions',
   }
-  
+  # resources :users, only: [:new, :show, :index, :edit, :update, :destroy]
   devise_for :users, controllers: {
     sessions: 'user/sessions',
     registrations: 'user/registrations',
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   }
   get '/' => 'homes#top'
   get '/about' => 'homes#about'
+  # get '/mypage' => 'user#'
   # resources :posts, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
