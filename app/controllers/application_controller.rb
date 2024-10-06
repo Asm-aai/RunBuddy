0d63bdfa9bfa4d9e6fc2d528f5e30a1f246ce209
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-10-06 07:18:09 +0000

    gestLogin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :index]
   before_action :authenticate_admin!, except: [:top]
+
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+
 end
