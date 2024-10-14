663c2c1113444f5c9c292593dc201c5ff1b440e1
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-10-14 14:57:20 +0000

    Complete

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,7 @@ class SearchesController < ApplicationController
   before_action :authenticate_user_or_admin!
 
   def search
+    @range = params[:range]
     @word = params[:word]
     @posts = Post.where("title LIKE ? OR introduction LIKE ?", "%#{@word}%", "%#{@word}%")
     @users = User.where("name LIKE ?", "%#{@word}%")
