f18b9d3ab5cc99a7a268bc87de25731496ba300e
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-10-19 11:17:25 +0000

    AddReviwe

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,9 +2,19 @@ class SearchesController < ApplicationController
   before_action :authenticate_user_or_admin!
 
   def search
-    @range = params[:range]
     @word = params[:word]
     @posts = Post.where("title LIKE ? OR introduction LIKE ?", "%#{@word}%", "%#{@word}%")
     @users = User.where("name LIKE ?", "%#{@word}%")
+
+    if params[:sort] == "created_at_asc"
+      @posts = @posts.order(created_at: :asc)
+    elsif params[:sort] == "created_at_desc"
+      @posts = @posts.order(created_at: :desc)
+    elsif params[:sort] == "favorited_desc"
+      @posts = @posts.left_joins(:favorites).group('posts.id').order('COUNT(favorites.id) DESC')
+    elsif params[:sort] == "comments_desc"
+      @posts = @posts.left_joins(:comments).group('posts.id').order('COUNT(comments.id) DESC')
+    end
+
   end
 end
--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -7,14 +7,6 @@ class User::CommentsController < UserApplicationController
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = @post.id
 
-    @comment.total_star = "1"
-    @comment.extent_star = "1"
-    @comment.clean_star = "1"
-    @comment.amenity_star = "1"
-    @comment.safety_star = "1"
-    @comment.cost_star = "1"
-    # 仮データ
-
     if @comment.save
       redirect_to post_path(@post), notice: 'コメントを投稿しました。'
     else
@@ -37,14 +29,6 @@ class User::CommentsController < UserApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
-    @comment.total_star = "1"
-    @comment.extent_star = "1"
-    @comment.clean_star = "1"
-    @comment.amenity_star = "1"
-    @comment.safety_star = "1"
-    @comment.cost_star = "1"
-    # 仮データ
-
   end
 
   def update
@@ -62,7 +46,6 @@ class User::CommentsController < UserApplicationController
 
   private
   def comment_params
-    # params.require(:comment).permit(:comment, :total_star, :extent_star, :clean_star, :amenity_star, :safety_star, :cost_star, :user_id, :post_id)
     params.require(:comment).permit(:comment, :user_id, :post_id, :total_star, :extent_star, :clean_star, :amenity_star, :safety_star, :cost_star)
   end
 
