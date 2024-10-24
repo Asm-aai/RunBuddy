4fc44420f4a654947fedc17c23babc60cb3b4f0a
Asm-aai <176456930+Asm-aai@users.noreply.github.com>
2024-10-24 02:34:02 +0000

    migrate

--- db/migrate/20241023152729_add_unique_index_to_posts.rb
+++ db/migrate/20241023152729_add_unique_index_to_posts.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToPosts < ActiveRecord::Migration[6.1]
   def change
-    add_index :posts, [:address, :hp], unique: true, name: 'index_posts_on_address_and_hp_unique'
+    add_index :posts, [:address, :hp], unique: true, name: 'index_posts_on_address_and_hp_unique', length: { address: 255 }
   end
 end
